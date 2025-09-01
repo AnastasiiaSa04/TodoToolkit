c56029f6bdf4d8d943b2b9a3f7a981809a86c454
Anastasiia <sulollarianastasiia@gmail.com>
2025-09-01 09:52:16 +0200

    add todoSlice and configure store for todo app

---
+++ src/redux/slices/basketSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  data: [],
+  count: 0,
+};
+const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {},
+});
+export default basketSlice.reducer;
---
+++ src/redux/slices/todoSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    data: [],
+    count: 0,
+}
+
+
+const todoSlice = createSlice ({
+    name: "todo",
+    initialState,
+    reducers: {},
+})
+
+
+export default todoSlice.reducer;
